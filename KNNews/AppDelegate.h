940e4611b8221ece2d3250f62fe3d03664a5627f
akun1103 <akun1103@qq.com>
2016-05-05 10:44:01 +0800

    Initial Commit

---
+++ KNNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KNNews
+//
+//  Created by emper on 16/5/5.
+//  Copyright © 2016年 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
